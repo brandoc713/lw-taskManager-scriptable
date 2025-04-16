870869adc60c5bb344936c3464c5e96c2ebfb128
Brandon Carido <brandon.carido@gmail.com>
2025-04-16 18:47:12 -0400

    Trying accessing iCloud files again

--- taskManager.js
+++ taskManager.js
@@ -1,6 +1,7 @@
 // icon-color: cyan; icon-glyph: magic;
 
 // Local test data
+/*
 let data = [
   {
     "class": "Chin",
@@ -13,6 +14,35 @@ let data = [
     "dueDate": "2026-04-20"
   }
 ];
+*/
+
+let fm = FileManager.iCloud();
+let folder = fm.joinPath(fm.documentsDirectory(), "TaskManager");
+let path = fm.joinPath(folder, "tasks.json");
+
+if (!fm.fileExists(path)) {
+	fm.writeString(path, JSON.stringify([], null, 2));
+}
+
+// Load and validate JSON data
+await fm.downloadFileFromiCloud(path);
+let raw = fm.readString(path);
+let data;
+
+// Throw some warnings if it doesn't work
+try {
+	data = JSON.parse(raw);
+	if (!Array.isArray(data)) throw new Error("Not an array");
+} catch (e) {
+	console.warn("invalid or corrupted data - resetting to fake assignment.");
+	data = [
+		{
+			"class": "Fake",
+			"description": "Assignment",
+			"dueDate": "2026-01-01"
+		}
+	];
+}
 
 // Sort tasks by due date
 data.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
